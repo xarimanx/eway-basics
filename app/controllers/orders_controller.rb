fd9860ba3865cdbb0d4267bf23e3e6621e5d9120
Aleksey Lavrukhin <alexudracul@gmail.com>
2015-12-11 10:11:32 +0200

    refactoring for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,15 @@
 class OrdersController < ApplicationController
+
   def create
-    if new_order.update_attributes(order_params)
-      redirect_to orders_url, success: 'Created!'
-    else
-      render :new
-    end
+    new_order.update_attributes(order_params) ? redirect_to(orders_url, flash: { success: 'Created!' }) : render(:new)
   end
 
   def update
-    if order.update_attributes(order_params)
-      redirect_to orders_url, success: 'Updated!'
-    else
-      render :edit
-    end
+    order.update_attributes(order_params) ? redirect_to(orders_url, flash: { success: 'Updated!' }) : render(:edit)
   end
 
   def destroy
-    order.destroy
-    redirect_to orders_url, success: 'Destroyed!'
+    order.destroy && redirect_to( orders_url, flash: { success: 'Destroyed!' })
   end
 
   private
