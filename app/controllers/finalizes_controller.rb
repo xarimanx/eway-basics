1be361cbf692f8654f2266faae4b9eac3165943d
Aleksey Lavrukhin <alexudracul@gmail.com>
2015-12-11 15:24:14 +0200

    clear

--- app/controllers/finalizes_controller.rb
+++ app/controllers/finalizes_controller.rb
@@ -19,31 +19,3 @@ class FinalizesController < ApplicationController
     @order ||= Order.find(params[:order_id])
   end
 end
-{
-  "AccessCode":"60CF3fZryW_vHZ1orbE7mhppeLg6CroLWn7WS4sRnnr39fz6PDcO3lnKAXJYv0bC9EKYmI8kWs_29eC8hJ7EsMJtnRCJLjETyAizEr_F7XeguWE1U1N1FvPKOrNJQYi_cFcUE-BddNHE6djze_LeeinvHcA==",
-  "AuthorisationCode":"936260",
-  "ResponseCode":"00",
-  "ResponseMessage":"A2000",
-  "InvoiceNumber":"",
-  "InvoiceReference":"",
-  "TotalAmount":1000,
-  "TransactionID":11954681,
-  "TransactionStatus":true,
-  "TokenCustomerID":null,
-  "BeagleScore":0,
-  "Options":[
-
-  ],
-  "Verification":{
-    "CVN":0,
-    "Address":0,
-    "Email":0,
-    "Mobile":0,
-    "Phone":0
-  },
-  "BeagleVerification":{
-    "Email":0,
-    "Phone":0
-  },
-  "Errors":null
-}
