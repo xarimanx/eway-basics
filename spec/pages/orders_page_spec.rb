758ea0fa1b713f4b06cc5a0f223d0c070cad8eb2
Aleksey Lavrukhin <alexudracul@gmail.com>
2016-01-12 16:16:49 +0200

    Orders#edit_page

--- spec/pages/orders_page_spec.rb
+++ spec/pages/orders_page_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Orders' do
+describe 'Orders', type: :feature do
   let(:browser) { @browser ||= Selenium::WebDriver.for :ff }
   let(:index_page) { OrdersIndexPage.new(browser) }
   after(:each) { browser.quit }
@@ -16,6 +16,25 @@ describe 'Orders' do
     it 'with errors' do
       new_page.create_order('test', 'a')
       expect(new_page.text).to include('Price is not a number')
+    end
+  end
+
+  context '#edit page' do
+    let(:edit_page) { OrderEditPage.new browser, false }
+    let!(:order) { create :order }
+    before(:each) { edit_page.navigate_to edit_order_checkout_url(order) }
+
+    it 'pay order' do
+      edit_page.pay_as_jrandom
+      expect(index_page.text).to include('Transaction Approved')
+    end
+
+    it 'wrong payment' do
+      edit_page.pay(card_holder: 'JOHN RANDOM',
+                    exp_month: '10',
+                    exp_year: '2020',
+                    card_cvn: '123')
+      expect(index_page.text).to include('Error: Invalid Card Number')
     end
   end
 end
