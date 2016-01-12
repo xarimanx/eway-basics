fa4ec8865e4e5c674470aacfe31f6454ba52c9a9
Aleksey Lavrukhin <alexudracul@gmail.com>
2016-01-12 16:16:07 +0200

    order_factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'factory_girl_rails'
 require 'page-object'
 require 'selenium/webdriver'
 
@@ -22,5 +23,9 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  config.infer_spec_type_from_file_location!
+  config.before(:each) do
+    default_url_options[:host] = 'lvh.me:3000'
+  end
+
+  config.include FactoryGirl::Syntax::Methods
 end
